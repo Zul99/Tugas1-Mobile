9935c48bb67c3753a379cf2134e8faf17b4303a1
Zulkifli Al-Amin Lothian <zulkiflilothian99@gmail.com>
2021-05-23 22:47:29 +0800

    Penambahan class ViewModel

--- app/src/main/java/id/ac/unhas/noteapp/Database.kt
+++ app/src/main/java/id/ac/unhas/noteapp/Database.kt
@@ -1,34 +1,13 @@
 package id.ac.unhas.noteapp
 
 import android.content.Context
-import android.provider.ContactsContract
-import androidx.lifecycle.LiveData
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [ContactsContract.CommonDataKinds.Note::class], exportSchema = false, version = 1)
+@Database(entities = [Note::class], exportSchema = false, version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
-
-    class NoteDao : id.ac.unhas.noteapp.NoteDao {
-        override fun getNotes(): LiveData<List<Note>> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun insertNote(note: Note) {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun deleteNote(note: Note) {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun updateNote(note: Note) {
-            TODO("Not yet implemented")
-        }
-
-    }
 
     companion object {
 
